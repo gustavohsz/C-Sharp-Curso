6c8157ec968a53b3f7c6749144cb6a0038e773b9
gusta <gusta@DESKTOP-H6CL150>
2022-09-01 19:10:58 -0300

    Adicionar arquivos de projeto.

---
+++ Ex_Propostos 2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Ex_Propostos_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int N2 = int.Parse(Console.ReadLine());
+
+            int max = 0;
+            int min = 0;
+            if (N2 > N)
+            {
+                max = N2;
+                min = N;
+            }
+            else
+            {
+                max = N;
+                min = N2;
+            }
+
+            if ( max%min == 0)
+            {
+                Console.WriteLine("Multiplos");
+            }
+            else
+            {
+                Console.WriteLine("Não multiplos");
+            }
+        }
+    }
+}
